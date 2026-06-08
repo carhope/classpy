b985795dfcb99b720f58f8759493b3da80892792
carhope <26_61@bssm.hs.kr>
2026-06-08 10:20:57 +0900

    그냥 리스트로 회문 구현

---
+++ pylist.py
@@ -0,0 +1,17 @@
+'''append()
+insert()
+extend()
+index()
+pop()
+remove()
+clear()
+count()
+sort()'''
+
+list1 = []
+text = "cha hiu mang"
+
+for i in text:
+    list1.append(i)
+for k in range(len(list1)):
+    print(list1.pop(),end ="")
